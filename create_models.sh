18fd368bf67cb0233415d786ab06103748faa812
Richard Reinisch (weave while true) <code@richardreinisch.com>
2025-10-17 11:49:40 +0200

    roman architect experiment

--- create_models.sh
+++ create_models.sh
@@ -1,3 +1,4 @@
+ollama pull dolphin-mistral
 ollama pull llama3.2:3b
 ollama pull qwen2.5-coder:7b
 ollama pull nomic-embed-text
@@ -6,3 +7,4 @@ ollama create jimmy_roar -f modelfiles/Modelfile-jimmy_roar
 ollama create neil_knows -f modelfiles/Modelfile-neil_knows
 ollama create sophia_akasha_lightray -f modelfiles/Modelfile-sophia_akasha_lightray
 ollama create dr_sigmund_leud -f modelfiles/Modelfile-dr_sigmund_leud
+ollama create marcus-vitruvius-pollio -f modelfiles/Modelfile-marcus-vitruvius-pollio
